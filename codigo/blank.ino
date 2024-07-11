538bc6fa3cfa40db6ea6bf79c714a6bc9b73b8ce
ManfredSG <125845691+ManfredSG@users.noreply.github.com>
2024-07-11 09:26:29 -0600

    Primer codigo de Arduino
    Se adjunta el primer código de Arduino en donde se hacen las primeras pruebas de funcionamiento.

--- codigo/blank.ino
+++ codigo/blank.ino
@@ -1,1 +1,15 @@
+#include <SoftwareSerial.h>
 
+SoftwareSerial Dta(4, 3);
+
+void setup() {
+  Serial.begin(9600);
+  Dta.begin(9600);
+}
+
+void loop() {
+  while (Dta.available()>0) {
+    byte gpsData=Dta.read();
+    Serial.write(gpsData);
+  }
+}
